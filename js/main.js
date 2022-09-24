2c222311c60b87c4b442c9854142592ff6f22198
moustafaramadan <95917190+moustafaramadan@users.noreply.github.com>
2022-09-24 18:42:18 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,30 +20,14 @@ var x =setInterval( function(){
 
 
 const form = document.querySelectorAll(".mail");
-const mail=document.getElementById("input");
+// const mail=document.getElementById("input");
 const mail2=document.getElementById("input2");
-const error= document.getElementById("invalid-mail");
-const valid= document.getElementById("valid-mail");
+// const error= document.getElementById("invalid-mail");
+// const valid= document.getElementById("valid-mail");
 const error2= document.getElementById("invalid-mail2");
 const valid2= document.getElementById("valid-mail2");
 
     form[0].addEventListener('submit', (e)=>{
-        let mailV=false;
-        if(mail.value===""){
-         mailV=false
-         error.style.visibility="visible"
-        } 
-     
-        if(mail.value!==""){
-         mailV=true;
-         valid.style.visibility="visible"
-        }  
-        if(mailV===false){
-            e.preventDefault()
-        }
-    })
-
-    form[1].addEventListener('submit', (e)=>{
         let mailV=false;
         if(mail2.value===""){
          mailV=false
@@ -58,6 +42,22 @@ const valid2= document.getElementById("valid-mail2");
             e.preventDefault()
         }
     })
+
+    // form[1].addEventListener('submit', (e)=>{
+    //     let mailV=false;
+    //     if(mail2.value===""){
+    //      mailV=false
+    //      error2.style.visibility="visible"
+    //     } 
+     
+    //     if(mail2.value!==""){
+    //      mailV=true;
+    //      valid2.style.visibility="visible"
+    //     }  
+    //     if(mailV===false){
+    //         e.preventDefault()
+    //     }
+    // })
 
 
 
